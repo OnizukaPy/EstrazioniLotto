cf9b290198ae6e2baee1d8c9b225f8efe5cfe158
onizuka-py <onizuka.host@zohomail.eu>
2022-11-04 11:27:57 +0100

    b

---
+++ prove.py
@@ -0,0 +1,11 @@
+from itertools import combinations, permutations, combinations_with_replacement
+
+numeri = [1, 2, 3]
+comb = list(combinations(numeri, 2))
+print(comb)
+
+comb = list(permutations(numeri, 2))
+print(comb)
+
+comb = list(combinations_with_replacement(numeri, 2))
+print(comb)
