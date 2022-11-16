f65a7265fbd6c681bfccca2af7e46a273cb54e6d
onizuka-py <onizuka.host@zohomail.eu>
2022-11-16 09:08:31 +0100

    alcuni aggiornamenti di funzioni

--- prove.py
+++ prove.py
@@ -1,14 +1,32 @@
-from itertools import combinations, permutations, combinations_with_replacement
+import random
 
-numeri = [1, 2, 3]
-comb = list(combinations(numeri, 2))
-print(comb)
 
-comb = list(permutations(numeri, 2))
-print(comb)
 
-comb = list(combinations_with_replacement(numeri, 2))
-print(comb)
+def trova_duplicati(lista):
 
-a = 1
-print(len(a))
+    nya_lista = set(lista)
+    counting = []
+    for element in nya_lista:
+        count = 0
+        for i in lista:
+            if element == i:
+                count += 1
+        counting.append([element, round(count/len(lista)*100, 1)])
+
+    return counting
+
+lista = []
+
+for i in range(90):
+    lista.append(random.randint(1, 90))
+
+#print(trova_duplicati(lista))
+
+a = [1,2,3,4,5,6,7]
+b = [2,3,4,5,6,7,7]
+
+c = a + b 
+
+a = range(90)
+print(a)
+
